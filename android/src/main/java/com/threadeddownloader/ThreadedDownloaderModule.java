3b8ba0f11e240587d1567f37662197d3766cdd7a
findhumane <root@findhumane.com>
2024-03-17 13:22:07 -0500

    feat: android implementation

--- android/src/main/java/com/threadeddownloader/ThreadedDownloaderModule.java
+++ android/src/main/java/com/threadeddownloader/ThreadedDownloaderModule.java
@@ -7,10 +7,27 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.bridge.ReactContext;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import android.util.Log;
 
 @ReactModule(name = ThreadedDownloaderModule.NAME)
 public class ThreadedDownloaderModule extends ReactContextBaseJavaModule {
   public static final String NAME = "ThreadedDownloader";
+  private static final int DEFAULT_MAX_THREADS = 10;
+  private static final String NEWLINE = System.lineSeparator();
 
   public ThreadedDownloaderModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -22,11 +39,86 @@ public class ThreadedDownloaderModule extends ReactContextBaseJavaModule {
     return NAME;
   }
 
-
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+  public void performThreadedDownload(final String url, final double timeoutSeconds, Promise promise) {
+    Log.d(NAME, "performThreadedDownload start; url: " + url);
+
+    // https://developer.android.com/develop/background-work/background-tasks/asynchronous/java-threads
+    synchronized (this) {
+      if (executorService == null) {
+        executorService = Executors.newFixedThreadPool(DEFAULT_MAX_THREADS, new ThreadFactory() {
+          private AtomicInteger threadCount = new AtomicInteger(1);
+
+          @Override
+          public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            thread.setName(NAME + "-" + threadCount.getAndIncrement());
+            return thread;
+          }
+        });
+      }
+    }
+
+    DownloaderThread downloader = new DownloaderThread(url, promise);
+    executorService.execute(downloader);
+  }
+
+  class DownloaderThread implements Runnable {
+    final String url;
+    final double timeoutSeconds;
+    final Promise promise;
+
+    DownloaderThread(final String url, final double timeoutSeconds, final Promise promise) {
+      this.url = url;
+      this.timeoutSeconds = timeoutSeconds;
+      this.promise = promise;
+    }
+
+    @Override
+    public void run() {
+      try {
+        Log.d(NAME, "performThreadedDownload DownloaderThread start; url: " + url);
+
+        URL urlObject = new URL(url);
+        HttpURLConnection httpConnection = (HttpURLConnection)urlObject.openConnection();
+
+        int timeoutMilliseconds = ((int)timeoutSeconds) * 1000;
+        httpConnection.setConnectTimeout(timeoutMilliseconds);
+        httpConnection.setReadTimeout(timeoutMilliseconds);
+
+        Log.d(NAME, "performThreadedDownload DownloaderThread connection opened");
+
+        httpConnection.setRequestMethod("GET");
+        int responseCode = httpConnection.getResponseCode();
+
+        Log.d(NAME, "performThreadedDownload DownloaderThread response code: " + responseCode);
+
+        boolean isSuccessful = responseCode >= 100 && responseCode < 400;
+        String response = null;
+        try (InputStreamReader isr = new InputStreamReader(isSuccessful ? httpConnection.getInputStream() : httpConnection.getErrorStream())) {
+          try (BufferedReader br = new BufferedReader(isr)) {
+            StringBuffer responseBuffer = new StringBuffer();
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+              responseBuffer.append(inputLine);
+              responseBuffer.append(NEWLINE);
+            }
+            response = responseBuffer.toString();
+          }
+        }
+
+        Log.d(NAME, "performThreadedDownload DownloaderThread finished reading response body");
+
+        if (isSuccessful) {
+          promise.resolve(response);
+        } else {
+          promise.reject(NAME + " Error", "Error " + responseCode + " -- " + response, new Error("Error " + responseCode));
+        }
+      } catch (Throwable t) {
+        Log.e(NAME, "performThreadedDownload DownloaderThread failed", t);
+        promise.reject(NAME + " Error", "Failed to download " + url + " -- " + t.getLocalizedMessage(), t);
+      }
+    }
   }
 }
